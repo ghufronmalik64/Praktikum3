18e0a74b567b1ad0328260207224d4dd907b5cb6
ghufronmalik64@gmail.com <ghufronmalik64@gmail.com>
2022-10-29 13:36:16 +0700

    menambahkan folder

---
+++ Menghitung Luas & Keliling Lingkaran/main.py
@@ -0,0 +1,8 @@
+print("Menghitung Luas dan Keliling Lingkaran")
+
+r = input("Masukkan jari-jari lingkaran:")
+pi = 3.14
+l = pi * int(r) * int(r)
+k = 2 * pi * int(r)
+print("Luas lingkaran:", l)
+print("Keliling lingkaran:", k)
