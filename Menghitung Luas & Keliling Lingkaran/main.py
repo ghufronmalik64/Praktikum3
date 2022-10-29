cac661fdab7d0d1488bf57f6d1b741310f077f84
ghufronmalik64@gmail.com <ghufronmalik64@gmail.com>
2022-10-29 14:37:13 +0700

    menambahkan image

--- Menghitung Luas & Keliling Lingkaran/main.py
+++ Menghitung Luas & Keliling Lingkaran/main.py
@@ -1,8 +1,14 @@
-print("Menghitung Luas dan Keliling Lingkaran")
+print("Nama     : Ghufron Malik")
+print("Kelas    : TI.22.B2")
+print("Program  : Menghitung Luas dan Keliling Lingkaran")
+print("=================================================")
 
 r = input("Masukkan jari-jari lingkaran:")
-pi = 3.14
-l = pi * int(r) * int(r)
-k = 2 * pi * int(r)
+phi = 3.14
+l = phi * int(r) * int(r)
+k = 2 * phi * int(r)
+
+print("=================================================")
+print("Hasil")
 print("Luas lingkaran:", l)
 print("Keliling lingkaran:", k)
