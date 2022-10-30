b4358051ffcd9bf05c231b149a4429bff77b3f2f
ghufronmalik64@gmail.com <ghufronmalik64@gmail.com>
2022-10-30 21:20:44 +0700

    menambahkan gambar

--- Project Praktikum Offline/belah ketupat.py
+++ Project Praktikum Offline/belah ketupat.py
@@ -1,16 +1,13 @@
-n = int (input ("Masukkan Nilai :"))
-print()
+print('Belah Ketupat')
 
-for i in range(n):
-    for j in range(n,i,-1):
-         print(" ",end = "")
-    for j in range(2*i+1):
-         print("*",end = "")
-    print()
-    
-for i in range(n-2,-1,-1):
-    for j in range(n,i,-1):
-         print(" ",end = "")
-    for j in range(2*i+1):
-         print("*",end = "")
-    print()
+
+print('{0:>11}'.format('*'))
+print('{0:>12}'.format('**'))
+print('{0:>13}'.format('***'))
+print('{0:>14}'.format('****'))
+print('{0:>15}'.format('*****'))
+print('{0:>14}'.format('****'))
+print('{0:>13}'.format('***'))
+print('{0:>12}'.format('**'))
+print('{0:>11}'.format('*'))
+
